fc412f636b8e982e7d4fbb23240308de6fa7b780
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2025-12-07 12:10:24 +0300

    Revert "fourth generation of form"
    This reverts commit 4d9d764164f2ffa3732de720872ded8e7a79cb4a.

--- reformer-tutorial/src/App.tsx
+++ reformer-tutorial/src/App.tsx
@@ -1,19 +1,11 @@
-import { CreditApplicationForm } from './features/credit-application';
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <div className="min-h-svh bg-gray-100 py-6">
-      <CreditApplicationForm
-        mode="create"
-        onSubmitSuccess={(data) => {
-          console.log('Заявка отправлена:', data);
-        }}
-        onSubmitError={(error) => {
-          console.error('Ошибка отправки:', error);
-        }}
-      />
+    <div className="flex min-h-svh flex-col items-center py-6">
+      <Button>Click me</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- reformer-tutorial/src/features/credit-application/index.ts
+++
@@ -1,4 +0,0 @@
-export { CreditApplicationForm } from './CreditApplicationForm';
-export type { CreditApplicationFormProps } from './CreditApplicationForm';
-export type { CreditApplicationForm as CreditApplicationFormData } from './model/types';
-export { useCreditApplicationForm } from './hooks/useCreditApplicationForm';
