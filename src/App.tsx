373c1c33b9ebd37387bf58ae2ac9b963b189bfcf
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2025-12-10 21:29:38 +0300

    Revert generated form by Qwen code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { InsuranceApplicationForm } from "./forms/insurance-application/insurance-application-form"
-
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center py-6">
-      <InsuranceApplicationForm />
+
     </div>
   )
 }
---
+++ src/components/ui/step-navigation/index.ts
@@ -0,0 +1,7 @@
+export { StepNavigation } from './StepNavigation';
+export type {
+  StepNavigationHandle,
+  StepNavigationProps,
+  StepNavigationConfig,
+  StepNavigationRenderState,
+} from './types';
