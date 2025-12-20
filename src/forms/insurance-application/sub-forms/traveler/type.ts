4dc69f20c9131354533d50aee2a3793485ba4251
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2025-12-20 12:09:14 +0300

    Revert Cloude try #3

--- src/forms/insurance-application/sub-forms/traveler/type.ts
+++
@@ -1,9 +0,0 @@
-export interface TravelerData {
-  lastName: string;
-  firstName: string;
-  middleName: string;
-  birthDate: string;
-  passportNumber: string;
-  passportExpiry: string;
-  citizenship: string;
-}
